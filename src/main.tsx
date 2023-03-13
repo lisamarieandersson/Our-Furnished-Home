b1fd967d7ada3e46605a0398a1d3bff833982ef5
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-03-13 11:54:33 +0100

    added themeprovider

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createRoutesFromElements, Route } from 'react-router';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
-import CheckoutPage from './pages/CheckoutPage';
-import EmptyBagPage from './pages/EmptyBagPage';
-import OrderConfirmationPage from './pages/OrderConfirmationPage';
-import ProductPage from './pages/ProductPage';
-import StartPage from './pages/StartPage';
+import { ThemeProvider } from "@mui/material/styles";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createRoutesFromElements, Route } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import CheckoutPage from "./pages/CheckoutPage";
+import EmptyBagPage from "./pages/EmptyBagPage";
+import OrderConfirmationPage from "./pages/OrderConfirmationPage";
+import ProductPage from "./pages/ProductPage";
+import StartPage from "./pages/StartPage";
+import { themeOptions } from "./theme";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route>
-      {/* <Route element={<AppLayout />}> */}
+    <ThemeProvider themeOptions={themeOptions}>
+      <Route>
+        {/* <Route element={<AppLayout />}> */}
         <Route index element={<StartPage />} />
         <Route path="products/:id" element={<ProductPage />} />
         <Route path="orderconfirmation" element={<OrderConfirmationPage />} />
-        <Route path="*" element={<h3>404 Not Found</h3>}/>
-      {/* </Route> */}
-      {/* <Route element={<CheckoutLayout />}> */}
+        <Route path="*" element={<h3>404 Not Found</h3>} />
+        {/* </Route> */}
+        {/* <Route element={<CheckoutLayout />}> */}
         <Route path="checkout" element={<CheckoutPage />} />
         <Route path="emptybag" element={<EmptyBagPage />} />
-      {/* </Route> */}
-    </Route>
+        {/* </Route> */}
+      </Route>
+    </ThemeProvider>
   )
-)
+);
 
 // function AppLayout() {
 //   return (
@@ -50,7 +54,7 @@ const router = createBrowserRouter(
 //   )
 // }
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
