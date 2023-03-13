fc56f7e8adb154db59f6fd6fd3a5ba883bd558a4
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-03-13 11:54:08 +0100

    added theme

---
+++ src/theme.ts
@@ -0,0 +1,55 @@
+import { ThemeOptions } from "@mui/material/styles";
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#e8dbce",
+    },
+    background: {
+      default: "#f1f0ef",
+      paper: "#f1f0ef",
+    },
+  },
+  typography: {
+    button: {
+      fontFamily: "Prata",
+    },
+    body2: {
+      fontFamily: "Josefin Sans",
+    },
+    body1: {
+      fontFamily: "Prata",
+    },
+    subtitle2: {
+      fontFamily: "Prata",
+    },
+    subtitle1: {
+      fontFamily: "Prata",
+    },
+    h6: {
+      fontFamily: "Prata",
+    },
+    h5: {
+      fontFamily: "Prata",
+    },
+    h4: {
+      fontFamily: "Prata",
+    },
+    h3: {
+      fontFamily: "Prata",
+    },
+    h2: {
+      fontFamily: "Prata",
+    },
+    h1: {
+      fontFamily: "Prata",
+    },
+    caption: {
+      fontFamily: "Josefin Sans",
+    },
+    overline: {
+      fontFamily: "Josefin Sans",
+    },
+  },
+};
