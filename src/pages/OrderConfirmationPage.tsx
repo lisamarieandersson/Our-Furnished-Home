9de44acec814e8129347be3391926959c4cf4269
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-03-14 12:57:43 +0100

    test with button

--- src/pages/OrderConfirmationPage.tsx
+++ src/pages/OrderConfirmationPage.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+
 function OrderConfirmationPage() {
-    return(
-        <div>
-            <h1>OrderConfirmationPage</h1>
-        </div>
-    )
+  const theme = useTheme();
+  return (
+    <div>
+      <h1 style={{ fontFamily: theme.typography.h1.fontFamily }}>
+        Order Confirmation Page
+      </h1>
+      <h2 style={{ fontFamily: theme.typography.caption.fontFamily }}>
+        Order Confirmation Page
+      </h2>
+      <Button variant="contained">ADD TO BAG</Button>
+    </div>
+  );
 }
 
 export default OrderConfirmationPage;
