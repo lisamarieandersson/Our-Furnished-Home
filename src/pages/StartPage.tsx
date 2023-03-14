2107ef93d0d0167db3ad38b3cac78d2beadf7a07
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-03-14 12:57:54 +0100

    added product card as test

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,16 +1,23 @@
+import ProductCard from "../ProductCard";
+import { theme } from "../theme";
+
 function StartPage() {
-    return(
-        <div>
-            {/* <AppHeader />
+  return (
+    <div>
+      {/* <AppHeader />
             <main>
                 <Gallery>
             </main>
             <Footer /> */}
-            <h1
-            
-            >Startpage</h1>
-        </div>
-    )
+      <h1 style={{ fontFamily: theme.typography.h1.fontFamily }}>Startpage</h1>
+      <ProductCard
+      // image={Product.image}
+      // title={Product.title}
+      // brand={Product.brand}
+      // price={Product.price}
+      />
+    </div>
+  );
 }
 
-export default StartPage
+export default StartPage;
