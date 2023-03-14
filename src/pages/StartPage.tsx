10fa7a16cbe54fbbc8bc759a2518a80d71d80619
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-03-14 14:05:56 +0100

    removed unused code

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -10,12 +10,7 @@ function StartPage() {
             </main>
             <Footer /> */}
       <h1 style={{ fontFamily: theme.typography.h1.fontFamily }}>Startpage</h1>
-      <ProductCard
-      // image={Product.image}
-      // title={Product.title}
-      // brand={Product.brand}
-      // price={Product.price}
-      />
+      <ProductCard />
     </div>
   );
 }
