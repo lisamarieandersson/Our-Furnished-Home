e1dac682f8301d6df0a1c04019b9cfa62f579a90
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-03-15 15:46:44 +0100

    adjusted width

--- src/components/OrderConfirmation.tsx
+++ src/components/OrderConfirmation.tsx
@@ -3,8 +3,17 @@ import { Box, Container, Divider, Typography } from "@mui/material";
 function OrderConfirmation() {
   return (
     <div>
-      <Container maxWidth="md" sx={{ marginTop: "3rem" }}>
-        <Box sx={{ display: "flex", justifyContent: "space-between", marginBottom: "0.5rem" }}>
+      <Container
+        maxWidth="md"
+        sx={{ marginTop: "3rem", width: { xs: "90%", md: "100%" } }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: "0.5rem",
+          }}
+        >
           <Typography variant="h6">Your order</Typography>
           <Typography variant="h6">#9574729</Typography>
         </Box>
