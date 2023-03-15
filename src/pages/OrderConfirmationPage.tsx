b9a8aef3257d93bd1e36bb88823adb8161b472cf
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-03-15 15:15:04 +0100

    added header and footer on orderconfirmationpage

--- src/pages/OrderConfirmationPage.tsx
+++ src/pages/OrderConfirmationPage.tsx
@@ -1,17 +1,15 @@
 import { Button } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import Footer from "../components/Footer";
+import HeaderMain from "../components/HeaderMain";
 
 function OrderConfirmationPage() {
   const theme = useTheme();
   return (
     <div>
-      <h1 style={{ fontFamily: theme.typography.h1.fontFamily }}>
-        Order Confirmation Page
-      </h1>
-      <h2 style={{ fontFamily: theme.typography.caption.fontFamily }}>
-        Order Confirmation Page
-      </h2>
-      <Button variant="contained">ADD TO BAG</Button>
+      <HeaderMain />
+      
+      <Footer />
     </div>
   );
 }
