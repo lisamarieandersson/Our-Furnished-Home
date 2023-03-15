f837cc5cfc5dcff82d21e39a4ff8bf45acb1c054
Moa Hedendahl <moahedendahl@gmail.com>
2023-03-15 15:55:45 +0100

    Changed styling, added close button

--- src/components/ShoppingBagCard.tsx
+++ src/components/ShoppingBagCard.tsx
@@ -1,5 +1,4 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Container, Typography } from "@mui/material";
-import { Box } from "@mui/system";
 import { products } from "../../data";
 
 function ShoppingBagCard() {
@@ -9,36 +8,44 @@ function ShoppingBagCard() {
                 <Card sx={{
                     maxWidth: 'xl',
                     display: 'flex',
-                    p: '0'
+                    margin: '1rem',
+                    boxShadow: 'none',
                 }}>
-                    <CardMedia sx={{
+                    <CardMedia component='img' image={product.image} sx={{
                         width: '10rem'
-                    }} component='img' image={product.image} />
-                    <Box>
+                    }}/>
+                    <Container sx={{
+                        padding: 0,
+                    }}>
                         <CardContent sx={{
                             padding: 0,
                             marginLeft: '1rem',
                             marginTop: '2rem',
                         }}>
-                            <Typography variant="h5" data-cy="product-title" sx={{
+                            <Typography variant="h6" data-cy="product-title" sx={{
                                 p: '0.2rem'
                             }}>{product.title}</Typography>
                         </CardContent>                            
-                        <Box sx={{
+                        <CardContent sx={{
+                                padding: 0,
+                                margin: '0.5rem 1rem',
+                        }}>
+                            <Typography variant="subtitle2">Quantity</Typography>   
+                        </CardContent>
+                        <Container sx={{
                             display: 'flex',
                             alignItems: 'center',
-                            justifyContent: 'space-between'
+                            justifyContent: 'space-between',
+                            padding: 0,
                         }}>
-                            <CardContent sx={{
-                                    padding: 0,
-                                    margin: '0.5rem 1rem',
-                            }}>
-                                <Typography>Quantity</Typography>   
-                            </CardContent>
-                            <CardActions>
-                                <Button variant='text' color="secondary" size="small" onClick={() => (product)}>-</Button>
+                            <CardActions sx={{ padding: 0}}>
+                                <Button variant='text' color="secondary" size="small" onClick={() => (product)}>
+                                    <span className="material-symbols-outlined">remove</span> 
+                                </Button>
                                     <Typography>{/*item.quantity*/}{'1'}</Typography>
-                                <Button variant='text' color="secondary" size="small" onClick={() => (product)}>+</Button>
+                                <Button variant='text' color="secondary" size="small" onClick={() => (product)}>
+                                    <span className="material-symbols-outlined">add</span>
+                                </Button>
                             </CardActions>
                             <CardContent sx={{
                                 padding: 0,
@@ -46,8 +53,17 @@ function ShoppingBagCard() {
                             }}>
                                 <Typography>{product.price} SEK</Typography>
                             </CardContent>
-                        </Box>
-                    </Box>
+                        </Container>
+                    </Container>
+                    <CardActions sx={{
+                        display: 'flex',
+                        alignItems: 'start',
+                        padding: 0,
+                    }}>
+                        <Button color="secondary" size="small">
+                            <span className="material-symbols-outlined" >close</span>
+                        </Button>
+                    </CardActions>
                 </Card>
             ))}
         </Container>
