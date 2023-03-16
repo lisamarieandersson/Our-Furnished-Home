5be50d374ff75e7f0ee6c05cb87236a722804470
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-03-16 13:52:43 +0100

    added link to checkoutpage
    Co-authored-by: Lisa Marie Andersson <lisamarie.andersson@medieinstitutet.se>

--- src/components/HeaderIcons.tsx
+++ src/components/HeaderIcons.tsx
@@ -1,11 +1,16 @@
-import { Badge, Box, SxProps, Theme } from "@mui/material";
+import { Badge, Box, SxProps, Link, Theme } from "@mui/material";
 import { theme } from "../theme";
+import { Link as RouterLink} from "react-router-dom";
 
 const iconStyle: SxProps<Theme> = {
   fontSize: { xs: "2rem", sm: "2.5rem" },
   cursor: "pointer",
   padding: { xs: "0rem", md: "0.3rem" },
 };
+
+const StyledLink = (props: any) => (
+  <Link component={RouterLink} underline="none" color="inherit" {...props} />
+);
 
 function HeaderIcons() {
   return (
@@ -16,14 +21,24 @@ function HeaderIcons() {
       <Box className="material-symbols-outlined" sx={iconStyle}>
         favorite
       </Box>
-        <Badge badgeContent={4} color="primary" sx={{ "& .MuiBadge-badge": { fontFamily: theme.typography.subtitle1.fontFamily } }}>
-          <Box
-            className="material-symbols-outlined"
-            sx={{ fontSize: { xs: "2rem", sm: "2.5rem" } }}
+        <StyledLink to="/checkout">
+          <Badge
+            badgeContent={4}
+            color="primary"
+            sx={{
+              "& .MuiBadge-badge": {
+                fontFamily: theme.typography.subtitle1.fontFamily,
+              },
+            }}
           >
-            shopping_bag
-          </Box>
-        </Badge>
+            <Box
+              className="material-symbols-outlined"
+              sx={{ fontSize: { xs: "2rem", sm: "2.5rem", cursor: "pointer" } }}
+            >
+              shopping_bag
+            </Box>
+          </Badge>
+        </StyledLink>
     </div>
   );
 }
