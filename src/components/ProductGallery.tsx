bd0504b1aef29e10a03ca318d6a4605bf9294203
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-03-16 17:33:24 +0100

    edited imports and products to product in link

--- src/components/ProductGallery.tsx
+++ src/components/ProductGallery.tsx
@@ -1,10 +1,12 @@
-import Box from "@mui/material/Box";
-import CardContent from "@mui/material/CardContent";
-import Container from "@mui/material/Container";
-import Link from "@mui/material/Link";
-import Paper from "@mui/material/Paper";
+import {
+  Box,
+  CardContent,
+  Container,
+  Link,
+  Paper,
+  Typography,
+} from "@mui/material";
 import { styled, SxProps, Theme } from "@mui/material/styles";
-import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Unstable_Grid2";
 import { Link as RouterLink } from "react-router-dom";
 import { products } from "../../data/index";
@@ -25,7 +27,7 @@ const Item = styled(Paper)(({ theme }) => ({
   "& img": {
     width: "100%",
     height: "auto",
-    objectFit: "cover", // Ensures that the image fills the available space while preserving its aspect ratio
+    objectFit: "cover",
   },
 }));
 
@@ -51,7 +53,7 @@ function ProductGallery() {
           {products.map((product) => (
             <Grid key={product.id} xs={12} sm={6} md={4} data-cy="product">
               <StyledLink
-                to={`/products/${product.id}/${product.title
+                to={`/product/${product.id}/${product.title
                   .toLowerCase()
                   .replace(/\s+/g, "-")}`}
               >
