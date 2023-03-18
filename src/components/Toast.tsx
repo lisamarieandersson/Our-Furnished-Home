c6b1a694008ef78db733371aba3b50f4e2bff24a
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-03-18 14:27:37 +0100

    added price and button on toast

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,17 +1,20 @@
 import { Theme } from "@emotion/react";
-import { Box, SxProps, Typography } from "@mui/material";
+import { Box, SxProps } from "@mui/material";
 import MuiAlert, { AlertProps } from "@mui/material/Alert";
 import Button from "@mui/material/Button";
 import Snackbar from "@mui/material/Snackbar";
 import Stack from "@mui/material/Stack";
 import * as React from "react";
-import { theme } from "../theme";
 import { products } from "../../data/index";
+import { theme } from "../theme";
 
 const toastStyling: SxProps<Theme> = {
-  width: "100%",
+  width: "20%",
   background: theme.palette.primary.main,
   color: theme.palette.text.primary,
+  textAlign: "center",
+  fontSize: "1rem",
+  justifyContent: "center",
 };
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
@@ -51,15 +54,25 @@ function Toast() {
           onClose={handleClose}
           severity="success"
         >
-          <Box>
-          <img style={ { width: "10%" }} src={products[0].image} alt={products[0].title} />
-          <h1>{products[0].title}</h1>
+          {"Product added to bag"}
+          <Box sx={{ display: "flex", alignItems: "center", gap: "0.5rem", marginTop: "0.5rem" }}>
+            <img
+              style={{ width: "40%", border: "1px solid #00000047", }}
+              src={products[0].image}
+              alt={products[0].title}
+            />
+            <h4>{products[0].title}</h4>
+            <h4>{products[0].price}SEK</h4>
           </Box>
+          <Button
+            variant="outlined"
+            color="secondary"
+            sx={{ marginTop: "0.7rem", width: "70%", fontSize: "0.8rem" }}
+          >
+            Go to bag
+          </Button>
         </Alert>
       </Snackbar>
-      <Alert sx={toastStyling} icon={false} severity="success">
-        Product added to bag
-      </Alert>
     </Stack>
   );
 }
