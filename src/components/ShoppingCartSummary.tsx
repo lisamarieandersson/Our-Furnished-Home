4ac1f0c9ecf9c0e98f4ce264a40f04ed3e7fca68
Moa Hedendahl <moahedendahl@gmail.com>
2023-03-20 09:49:42 +0100

    Changed so we use mui components and added styling to match cart

--- src/components/ShoppingCartSummary.tsx
+++ src/components/ShoppingCartSummary.tsx
@@ -1,13 +1,31 @@
+import { Box, Container, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { useShoppingCart } from "../contexts/ShoppingCartContext";
 
 function ShoppingCartSummary() {
   const { totalItems, totalPrice } = useShoppingCart();
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
-    <div>
-      <p>Total items: {totalItems}</p>
-      <p>Total price: {totalPrice}</p>
-    </div>
+    <Container maxWidth= {isSmallScreen ? 'sm' : 'md'} >
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        margin: '0 1rem'
+      }}>
+        <Typography>Total items: </Typography>
+        <Typography>{totalItems}</Typography>
+      </Box>
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        margin: '0 1rem'
+      }}>
+        <Typography>Total price: </Typography>
+        <Typography>{totalPrice} SEK</Typography>
+      </Box>
+      
+    </Container>
   );
 }
 
