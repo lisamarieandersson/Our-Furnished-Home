210dca4d3aae6422020a1a0c243746d4d0921a6b
Moa Hedendahl <moahedendahl@gmail.com>
2023-03-20 18:06:40 +0100

    Began with ordercontext, tried two different ways

---
+++ src/contexts/OrderContext.tsx
@@ -0,0 +1,187 @@
+// import React, { createContext, useContext, useState } from "react";
+// import { CartItem } from "../../data";
+
+// // Define Props interface to enforce the type of the `children` prop
+// interface Props {
+//   children: React.ReactNode;
+// }
+
+// type Order = {
+//     items: CartItem[];
+//     totalItems: number;
+//     totalPrice: number;
+//     orderNumber: string;
+//     name: string;
+//     email: string;
+//     address: string;
+//     phone: string;
+//     city: string;
+//     postalcode: string;
+// }
+
+// type ShoppingCart = {
+//   items: CartItem[];
+//   addItem: (item: CartItem) => void;
+//   removeItem: (id: string) => void;
+//   clearCart: () => void;
+//   totalItems: number;
+//   totalPrice: number;
+// };
+
+// type OrderContextType = {
+//     order: Order;
+//     setOrder: (order: Order) => void;
+//     clearOrder: () => void;
+// } & ShoppingCart;
+
+// // Create a new context object with an empty shopping cart object as its initial value
+// const OrderContext = createContext<OrderContextType>({
+//     items: [],
+//     addItem: () => {},
+//     removeItem: () => {},
+//     clearCart: () => {},
+//     totalItems: 0,
+//     totalPrice: 0,
+//     order: {
+//         items: [],
+//         totalItems: 0,
+//         totalPrice: 0,
+//         orderNumber: "",
+//         name: "",
+//         email: "",
+//         address: "",
+//         phone: "",
+//         city: "",
+//         postalcode: "",
+//     },
+//     setOrder: () => {},
+//     clearOrder: () => {},
+// })
+
+// // Create a custom hook to easier use the shopping cart
+// export const useOrder = () => useContext(OrderContext);
+
+// // Create a new component that provides the shopping cart context to its child components
+// export const ShoppingCartProvider = ({ children }: Props) => {
+//   // Initialize a state variable for the items in the shopping cart
+//   const [items, setItems] = useState<CartItem[]>([]);
+
+//   const addItem = (itemToAdd: CartItem) => {
+//     const existingItem = items.find((item) => item.id === itemToAdd.id); // Check if the item to be added already exists in the cart by finding an item with the same id
+
+//     if (existingItem) {
+//       // If the item already exists in the cart, increase the quantity of that item
+//       const updatedItems = items.map((item) => {
+//         // Create a new array of items by iterating over the existing items in the cart
+//         if (item.id === existingItem.id) {
+//           // If the id of the current item matches the id of the existing item, update the quantity
+//           return { ...item, quantity: item.quantity + itemToAdd.quantity };
+//         } else {
+//           return item; // Otherwise, return the current item as-is
+//         }
+//       });
+//       setItems(updatedItems); // Update the items in the cart with the updatedItems array
+//     } else {
+//       // If the item does not exist in the cart, add it as a new item
+//       setItems([...items, itemToAdd]); // Create a new array of items that includes the existing items and the new item, and update the items in the cart with the new array
+//     }
+
+//     console.log("adding product");
+//   };
+
+//   const removeItem = (id: string) => {
+//     // TODO: implement removeItem function
+//   };
+
+//   const clearCart = () => {
+//     // TODO: implement clearCart function
+//   };
+
+//   const totalItems = items.reduce((total, item) => total + item.quantity, 0);
+//   console.log(totalItems);
+//   const totalPrice = items.reduce(
+//     (total, item) => total + item.price * item.quantity,
+//     0
+//   );
+
+//   // Create a shopping cart object with all necessary properties and methods
+//   const shoppingCart: ShoppingCart = {
+//     items,
+//     addItem,
+//     removeItem,
+//     clearCart,
+//     totalItems,
+//     totalPrice,
+//   };
+
+//   // Render the child components wrapped inside the ShoppingCartContext.Provider
+//   return (
+//     <OrderContext.Provider value={shoppingCart}>
+//       {children}
+//     </OrderContext.Provider>
+//   );
+// };
+
+import React, { createContext, useContext, useState } from "react";
+import { CartItem } from "../../data";
+import { useShoppingCart } from "./ShoppingCartContext";
+
+// Define Props interface to enforce the type of the `children` prop
+interface Props {
+  children: React.ReactNode;
+}
+
+interface Order {
+  orderNumber: string;
+  products: CartItem[];
+  address: string;
+  email: string;
+  name: string;
+  city: string;
+  postalcode: string;
+  phonenumber: string;
+}
+
+type OrderContextType = {
+  order: Order | null;
+  setOrder: (order: Order) => void;
+};
+
+// Create a new context object with an initial value of null for the order
+const OrderContext = createContext<OrderContextType>({
+  order: null,
+  setOrder: () => {},
+});
+
+// Create a custom hook to easier use the order
+export const useOrder = () => useContext(OrderContext);
+
+// Create a new component that provides the order context to its child components
+export const OrderProvider = ({ children }: Props) => {
+  const [order, setOrder] = useState<Order | null>(null);
+  const { items, totalPrice } = useShoppingCart();
+
+  // Define a function to create the order object based on the current shopping cart state and delivery address
+  const createOrder = (address: string,
+    email: string,
+    name: string,
+    city: string,
+    postalcode: string,
+    phonenumber: string,) => {
+    const orderNumber = `ORD-${Math.floor(Math.random() * 100000)}`; // Generate a random order number
+    const products = items; // Get the list of products from the shopping cart
+    const newOrder = { orderNumber, products, address, email, name, city, postalcode, phonenumber }; // Create the order object with the generated order number, products, and delivery address
+
+    setOrder(newOrder); // Update the order state with the new order
+  };
+
+  // Create an object with all necessary properties and methods for the OrderContext
+  const orderContext: OrderContextType = {
+    order,
+    setOrder,
+    createOrder,
+  };
+
+  // Return the OrderContext.Provider component with the value prop set to the orderContext object
+  return <OrderContext.Provider value={orderContext}>{children}</OrderContext.Provider>;
+};
