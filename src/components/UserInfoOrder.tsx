f69c4089f644b0efec51a4fb743d77eba3ed2ee4
Moa Hedendahl <moahedendahl@gmail.com>
2023-03-21 14:08:29 +0100

    changed some variables

--- src/components/UserInfoOrder.tsx
+++ src/components/UserInfoOrder.tsx
@@ -1,45 +1,61 @@
 import { Button, Typography, useMediaQuery } from "@mui/material";
 import { Container, useTheme } from "@mui/system";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { useShoppingCart } from "../contexts/ShoppingCartContext";
 
 function UserInfoOrder() {
-    const theme = useTheme();
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
-    const { items } = useShoppingCart();
-    
-    return (
-        <Container maxWidth= {isSmallScreen ? 'sm' : 'md'} sx={{
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: 'column',
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const { items } = useShoppingCart();
+
+  return (
+    <Container
+      maxWidth={isSmallScreen ? "sm" : "md"}
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}>
+      <Typography variant={isSmallScreen ? "h6" : "h5"}>
+        Thank you for your order!
+      </Typography>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "1rem",
         }}>
-            <Typography variant={isSmallScreen ? 'h6' : 'h5'}>Thank you for your order!</Typography>
-            <Container sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                marginTop: '1rem',
-            }}>
-                <Typography textAlign={'center'}>An order confirmation has been sent to:</Typography>
-                <Typography>{/*ordercontext.email typ eller nått*/}johan.johansson@medieinstitutet.se</Typography>
-            </Container>
-            <Typography variant={isSmallScreen ? 'h6' : 'h5'} marginTop={'2rem'}>Your order details:</Typography>
-            <Container sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                marginTop: isSmallScreen ? '0.2rem' : '1rem',
-                marginBottom: isSmallScreen ? '1rem' : '3rem',
-            }}>
-                <Typography>{/*Ordercontext.namn*/}Johan Johansson</Typography>
-                <Typography>{/*Ordercontext.address*/}VägenVägen 12</Typography>
-                <Typography>{/*Ordercontext.postalcode*/}12345 {/*Ordercontext.city*/} Göteborg</Typography>
-                <Typography>{/*Ordercontext.phonenumber*/}072 1234567</Typography>
-            </Container>
-            <Button component={Link} to={'/'} variant="contained">Continue to shop</Button>
-        </Container>
-    )
+        <Typography textAlign={"center"}>
+          An order confirmation has been sent to:
+        </Typography>
+        <Typography>
+          {/*order.email typ eller nått*/}johan.johansson@medieinstitutet.se
+        </Typography>
+      </Container>
+      <Typography variant={isSmallScreen ? "h6" : "h5"} marginTop={"2rem"}>
+        Your order details:
+      </Typography>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: isSmallScreen ? "0.2rem" : "1rem",
+          marginBottom: isSmallScreen ? "1rem" : "3rem",
+        }}>
+        <Typography>{/*order.namn*/}Johan Johansson</Typography>
+        <Typography>{/*order.address*/}VägenVägen 12</Typography>
+        <Typography>
+          {/*order.postalcode*/}12345 {/*order.city*/} Göteborg
+        </Typography>
+        <Typography>{/*order.phonenumber*/}072 1234567</Typography>
+      </Container>
+      <Button component={Link} to={"/"} variant="contained">
+        Continue to shop
+      </Button>
+    </Container>
+  );
 }
 
 export default UserInfoOrder;
