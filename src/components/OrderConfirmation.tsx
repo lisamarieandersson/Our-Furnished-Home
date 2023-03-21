ce648ad31e139dc93d58bfb47dec339ab6469b0f
Moa Hedendahl <moahedendahl@gmail.com>
2023-03-21 10:33:11 +0100

    Made order confirmation card, added necessary content

--- src/components/OrderConfirmation.tsx
+++ src/components/OrderConfirmation.tsx
@@ -1,25 +1,76 @@
-import { Box, Container, Divider, Typography } from "@mui/material";
+import { Box, Card, CardContent, CardMedia, Container, Divider, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { useShoppingCart } from "../contexts/ShoppingCartContext";
 
 function OrderConfirmation() {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const { items } = useShoppingCart();
+  
   return (
-    <div>
-      <Container
-        maxWidth="md"
-        sx={{ marginTop: "3rem", width: { xs: "90%", md: "100%" } }}
+    <Container
+      maxWidth= {isSmallScreen ? 'sm' : 'md'}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          margin: "1rem",
+        }}
       >
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            marginBottom: "0.5rem",
-          }}
-        >
-          <Typography variant="h6">Your order</Typography>
-          <Typography variant="h6">#9574729</Typography>
-        </Box>
-        <Divider></Divider>
-      </Container>
-    </div>
+        <Typography variant="h6">Your order</Typography>
+        <Typography variant="h6">#9574729</Typography>
+      </Box>
+      <Divider sx={{backgroundColor: theme.palette.primary.main, marginBottom: '1rem',}}></Divider>
+      {items.map((CartItem) => (
+        <Card 
+            key = {CartItem.id}
+            sx={{
+                display: 'flex',
+                margin: '1rem',
+                boxShadow: 'none',
+                position: 'relative',
+            }}>
+            <CardMedia component='img' image={CartItem.image} sx={{
+                width: isSmallScreen ? '6rem' : '10rem',
+            }}/>
+            <Container sx={{
+                padding: '0px !important',
+                display: 'flex',
+                flexDirection: 'column',
+            }}>
+              <Container sx={{
+                padding: '0px !important',
+                marginTop: '1rem',
+              }}>
+                <CardContent sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  textAlign: 'center',
+                  padding: '0px !important',
+                  marginLeft: '1rem',
+                  marginTop: '2rem',
+                }}>
+                  <Typography variant= {isSmallScreen ? 'body1': 'h6'}>
+                    {CartItem.title}
+                  </Typography>
+                  <Typography variant= {isSmallScreen ? 'body2': 'body1'}>
+                    {CartItem.price} SEK
+                  </Typography>
+                </CardContent>
+              </Container>
+                <CardContent sx={{
+                  padding: '0px !important',
+                  marginLeft: '1rem'
+                }}>
+                  <Typography fontSize={'0.8rem'}>
+                    Quantity: {CartItem.quantity}
+                  </Typography>
+                </CardContent>
+            </Container>
+        </Card>))}
+        <Divider sx={{backgroundColor: theme.palette.primary.main, marginBottom: '1rem',}}></Divider>
+    </Container>
   );
 }
 
