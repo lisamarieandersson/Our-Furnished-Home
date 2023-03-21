c14ec8c857b64e95b6326fe572c82e937eb1bc2a
Moa Hedendahl <moahedendahl@gmail.com>
2023-03-21 13:50:41 +0100

    Fixed ordercontext and made it work with form

--- src/contexts/OrderContext.tsx
+++ src/contexts/OrderContext.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useState } from "react";
 import { CartItem } from "../../data";
+import { DeliveryValues } from "../components/DeliveryForm";
 import { useShoppingCart } from "./ShoppingCartContext";
 
 // Define Props interface to enforce the type of the `children` prop
@@ -10,9 +11,9 @@ interface Props {
 interface Order {
   orderNumber: string;
   products: CartItem[];
-  address: string;
   email: string;
   name: string;
+  address: string;
   city: string;
   postalcode: string;
   phonenumber: string;
@@ -21,14 +22,14 @@ interface Order {
 type OrderContextType = {
   order: Order | null;
   setOrder: (order: Order) => void;
-  createOrder: (address: string, email: string, name: string, city: string, postalcode: string, phonenumber: string) => void;
+  createOrder: (deliveryValues: DeliveryValues) => void;
 };
 
 // Create a new context object with an initial value of null for the order
 const OrderContext = createContext<OrderContextType>({
   order: null,
   setOrder: () => {},
-  createOrder: () => {}, //VEt inte om det är rätt!!!!!
+  createOrder: () => {},
 });
 
 // Create a custom hook to easier use the order
@@ -40,15 +41,14 @@ export const OrderProvider = ({ children }: Props) => {
   const { items, totalPrice } = useShoppingCart();
 
   // Define a function to create the order object based on the current shopping cart state and delivery address
-  const createOrder = (address: string,
-    email: string,
-    name: string,
-    city: string,
-    postalcode: string,
-    phonenumber: string,) => {
+  const createOrder = (deliveryValues: DeliveryValues) => {
     const orderNumber = `ORD-${Math.floor(Math.random() * 100000)}`; // Generate a random order number
     const products = items; // Get the list of products from the shopping cart
-    const newOrder = { orderNumber, products, address, email, name, city, postalcode, phonenumber }; // Create the order object with the generated order number, products, and delivery address
+    const newOrder: Order = {
+      orderNumber,
+      products,
+      ...deliveryValues,
+    }; // Create the order object with the generated order number, products, and delivery address
 
     setOrder(newOrder); // Update the order state with the new order
   };
@@ -61,5 +61,9 @@ export const OrderProvider = ({ children }: Props) => {
   };
 
   // Return the OrderContext.Provider component with the value prop set to the orderContext object
-  return <OrderContext.Provider value={orderContext}>{children}</OrderContext.Provider>;
+  return (
+    <OrderContext.Provider value={orderContext}>
+      {children}
+    </OrderContext.Provider>
+  );
 };
