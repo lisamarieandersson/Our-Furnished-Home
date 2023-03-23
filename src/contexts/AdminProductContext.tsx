22755c9437085b35a8aa05d8faf8f8f5ba61a336
Moa Hedendahl <moahedendahl@gmail.com>
2023-03-23 10:31:15 +0100

    Changed some names and added a createproduct function

--- src/contexts/AdminProductContext.tsx
+++ src/contexts/AdminProductContext.tsx
@@ -1,5 +1,6 @@
-import React, { ContextType, createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { Product } from "../../data";
+import { ProductValues } from "../components/AddProductForm";
 
 // Define Props interface to enforce the type of the `children` prop
 interface Props {
@@ -30,15 +31,14 @@ const ProductContext = createContext<ProductContextType>({
   editItem: () => {},
 });
 
-export const MyContext = createContext<ContextType>(initialContext);
-
-export const useMyContext = () => useContext(MyContext);
+// Create a custom hook to easier use the order
+export const useProductContext = () => useContext(ProductContext);
 
 interface Props {
   children: React.ReactNode;
 }
 
-export const MyProvider = ({ children }: Props) => {
+export const ProductProvider = ({ children }: Props) => {
   const [items, setItems] = useState<Item[]>([]);
 
   const addItem = (item: Item) => {
@@ -55,6 +55,8 @@ export const MyProvider = ({ children }: Props) => {
     );
   };
 
+  const createProduct = (productValues: ProductValues) => {};
+
   const contextValue = {
     items,
     addItem,
@@ -63,6 +65,8 @@ export const MyProvider = ({ children }: Props) => {
   };
 
   return (
-    <MyContext.Provider value={contextValue}>{children}</MyContext.Provider>
+    <ProductContext.Provider value={contextValue}>
+      {children}
+    </ProductContext.Provider>
   );
 };
