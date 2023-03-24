2207f550a951d379581ba313246dfb73239c13d5
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-03-24 17:45:41 +0100

    removed div that caused error in the console

--- src/components/DeleteProductDialog.tsx
+++ src/components/DeleteProductDialog.tsx
@@ -14,34 +14,32 @@ type Props = {
 
 function DeleteProductDialog({ open, handleClose }: Props) {
   return (
-    <div>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{"Delete product"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Are you sure you want to delete this product?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button variant="contained" onClick={handleClose}>
-            No
-          </Button>
-          <Button
-            variant="contained"
-            data-cy="confirm-delete-button"
-            onClick={handleClose}
-            autoFocus
-          >
-            Yes
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{"Delete product"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          Are you sure you want to delete this product?
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="contained" onClick={handleClose}>
+          No
+        </Button>
+        <Button
+          variant="contained"
+          data-cy="confirm-delete-button"
+          onClick={handleClose}
+          autoFocus
+        >
+          Yes
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
 
