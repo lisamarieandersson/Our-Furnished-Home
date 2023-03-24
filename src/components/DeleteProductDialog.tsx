bbb3627bcc35d53cd178055501a963d53afdc29f
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-03-24 13:29:53 +0100

    added props for open and handlClose

--- src/components/DeleteProductDialog.tsx
+++ src/components/DeleteProductDialog.tsx
@@ -1,27 +1,20 @@
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 
-function DeleteProductDialog() {
-  const [open, setOpen] = useState(false);
+type Props = {
+  open: boolean;
+  handleClose: () => void;
+};
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
+function DeleteProductDialog({ open, handleClose }: Props) {
   return (
     <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Open alert dialog
-      </Button>
       <Dialog
         open={open}
         onClose={handleClose}
@@ -38,7 +31,12 @@ function DeleteProductDialog() {
           <Button variant="contained" onClick={handleClose}>
             No
           </Button>
-          <Button variant="contained" onClick={handleClose} autoFocus>
+          <Button
+            variant="contained"
+            data-cy="confirm-delete-button"
+            onClick={handleClose}
+            autoFocus
+          >
             Yes
           </Button>
         </DialogActions>
