6fbba28b653fb50cf9757b8e4abe92b9d96f4250
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-03-29 15:16:09 +0200

    Added comment for function Co-authored-by: Moa Hedendahl <moa.hedendahl@medieinstitutet.se> Co-authored-by: Nathalie Gustafsson <nathaliegustafsson@medieinstitutet.se>

--- src/slug/utils.ts
+++ src/slug/utils.ts
@@ -1,8 +1,7 @@
 /**
- *
  * @param title
- * @returns Takes a string (title) as input and returns a URL-friendly slug
- * by converting it to lowercase, normalizing diacritic characters, and replacing non-alphanumeric
+ * @returns Takes a string (title) as input and returns a URL-friendly slug by converting
+ * it to lowercase, normalizing diacritic characters, and replacing non-alphanumeric
  * characters with hyphens while removing any leading or trailing hyphens.
  */
 export function createSlug(title: string): string {
