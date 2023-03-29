e6eb6953a32ea7a8c3b31ca22d502ec305b38ff0
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-03-29 15:18:31 +0200

    Added comments for Toast Co-authored-by: Moa Hedendahl <moa.hedendahl@medieinstitutet.se> Co-authored-by: Nathalie Gustafsson <nathaliegustafsson@medieinstitutet.se>

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -7,17 +7,7 @@ import Stack from "@mui/material/Stack";
 import React from "react";
 import { Link } from "react-router-dom";
 import { CartItem } from "../../data/index";
-import { theme } from "../theme";
-
-const toastStyling: SxProps<Theme> = {
-  background: theme.palette.primary.main,
-  color: theme.palette.text.primary,
-  textAlign: "center",
-  fontSize: "1rem",
-  "& .MuiAlert-action": {
-    padding: 0,
-  },
-};
+import { theme } from "../theme/theme";
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
   props,
@@ -26,6 +16,10 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
+/**
+ * Renders a toast that pops up when the user adds a product to the shopping bag.
+ * Showing image, title and price * quantity of the items and a "go to bag"-button.
+ */
 function Toast({
   open,
   onClose,
@@ -39,7 +33,7 @@ function Toast({
 }) {
   return (
     <Stack spacing={2} sx={{ width: "20%" }}>
-      <Snackbar 
+      <Snackbar
         data-cy="added-to-cart-toast"
         open={open}
         autoHideDuration={6000}
@@ -75,19 +69,32 @@ function Toast({
               <h4>{lastAddedItem?.price} SEK</h4>
             </Box>
           </Box>
-            <Button
-              component={Link} to="/checkout"
-              onClick={clearLastAddedItem}
-              variant="outlined"
-              color="secondary"
-              sx={{ marginTop: "0.7rem", width: "80%", fontSize: "0.8rem" }}
-            >
-              Go to bag
-            </Button>
+          <Button
+            component={Link}
+            to="/checkout"
+            onClick={clearLastAddedItem}
+            variant="outlined"
+            color="secondary"
+            sx={{ marginTop: "0.7rem", width: "80%", fontSize: "0.8rem" }}
+          >
+            Go to bag
+          </Button>
         </Alert>
       </Snackbar>
     </Stack>
   );
 }
+
+/* Styling */
+
+const toastStyling: SxProps<Theme> = {
+  background: theme.palette.primary.main,
+  color: theme.palette.text.primary,
+  textAlign: "center",
+  fontSize: "1rem",
+  "& .MuiAlert-action": {
+    padding: 0,
+  },
+};
 
 export default Toast;
